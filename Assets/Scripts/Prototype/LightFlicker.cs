4d27da3aa714f6349f1ebee9d3376189d89cbb22
William Terry <Tezza48is@aol.com>
2016-10-17 01:13:24 +0100

    Lights flicker now, need to make an override for scamper to use

---
+++ Assets/Scripts/Prototype/LightFlicker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+    float initialIntensity;
+    bool triggered = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        initialIntensity = GetComponent<Light>().intensity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!triggered)
+        {
+            int chance = Random.Range(0, 50);
+            if (chance == 1)
+            {
+                GetComponent<Animator>().SetTrigger("Start");
+                triggered = true;
+            }
+        }
+	}
+}
